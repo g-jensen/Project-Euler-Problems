ff0c5d09eae79dbc72cbe3bea84362e8209df243
Greg Jensen <77466937+g-jensen@users.noreply.github.com>
2021-11-16 22:27:40 -0500

    Create 021.cpp

---
+++ cpp/021.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int amicate (int n) {
+    int sum = 0;
+    for (int i = 1; i <= (n/2)+1; i++) {
+        if (n % i == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+bool isAmicable(int a) {
+    int b = amicate(a);
+    return amicate(b) == a && a != b;
+}
+
+int main () {
+    std::vector<int> dupe;
+    int sum = 0;
+    for (int i = 1; i < 10000; i++) {
+        if (isAmicable(i)) {
+            sum += i;
+        }
+    }
+
+    std::cout << sum << std::endl;
+}
