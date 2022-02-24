990b7048483f2303c3dc41f8c2a16357c56769c1
Greg Jensen <77466937+g-jensen@users.noreply.github.com>
2022-02-23 19:00:43 -0500

    Update 018.cpp

--- cpp/018.cpp
+++ cpp/018.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
-#include <algorithm>
-int output[16384] = {0};
-int index = 0;
-void getPathValue(int tree[15][15], size_t i, size_t k, size_t acc) {
-    if (i < 14 && k < 14) {
+size_t getPathValue(int tree[15][15], size_t i, size_t k, size_t acc, size_t& highest) {
+    if (i < 15 && k < 15) {
         i++;
-        int l = tree[i][k];
-        int r = tree[i][k+1];
-        getPathValue(tree,i,k,acc+l);
-        getPathValue(tree,i,k+1,acc+r);
-    } else {
-        output[index] = acc;
-        index++;
+        if (acc > highest) {highest = acc;}
+        getPathValue(tree,i,k,acc+tree[i][k],highest);
+        getPathValue(tree,i,k+1,acc+tree[i][k+1],highest);
     }
+    return highest;
 }
 int main () {
     int tree[15][15] = {{75,0,0,0,0,0,0,0,0,0,0,0,0,0,0},{95, 64,0,0,0,0,0,0,0,0,0,0,0,0,0},{17, 47, 82,0,0,0,0,0,0,0,0,0,0,0,0},{18, 35, 87, 10,0,0,0,0,0,0,0,0,0,0,0},{20, 4, 82, 47, 65,0,0,0,0,0,0,0,0,0,0},{19, 1, 23, 75, 3, 34,0,0,0,0,0,0,0,0,0},{88, 2, 77, 73, 7, 63, 67,0,0,0,0,0,0,0,0},{99, 65, 4, 28, 6, 16, 70, 92,0,0,0,0,0,0,0},{41, 41, 26, 56, 83, 40, 80, 70, 33,0,0,0,0,0,0},{41, 48, 72, 33, 47, 32, 37, 16, 94, 29,0,0,0,0,0},{53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14,0,0,0,0},{70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57,0,0,0},{91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48,0,0},{63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31,0},{4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23}};
-    getPathValue(tree,0,0,75);
-    std::cout << *std::max_element(output,output+16384) << std::endl;
+    size_t h = 75;
+    std::cout << getPathValue(tree,0,0,75,h) << std::endl;
 }
